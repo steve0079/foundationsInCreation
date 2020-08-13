76abe060889b42e08488239e4ee80b7d49bf16be
steve0079 <49959235+steve0079@users.noreply.github.com>
2020-08-13 16:51:41 -0700

    fixed css for navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = props => (
 
     <header className='toolbar'>
         <nav className='toolbar_navigation'>
-        <div className='toolbar_toggle-button'>
+            <div className='toolbar_toggle-button'>
                 <DrawerToggleButton click={props.drawerClickHandler} />
             </div>
             <div className="toolbar_logo">
@@ -23,12 +23,12 @@ const Navbar = props => (
                     <a href="https://www.instagram.com/gayzambrano/" target="_self" rel="noopener noreferrer"><i className="fab fa-instagram fa-2x"></i></a>
                 </div>
                 <div className="navItems">
-                    <AnchorLink  offset='100' href='#CustomerSlider' className="item">HOME</AnchorLink>
-                    <AnchorLink  offset='80' href='#AboutUs' className="item">ABOUT US</AnchorLink>
-                    <AnchorLink  offset='80' href='#Equipping' className="item">EQUIPPING</AnchorLink>
-                    <AnchorLink  offset='80' href='#Evangelism' className="item">EVENT SPEAKING</AnchorLink>
-                    <AnchorLink  offset='80' href='#TwoFourTwoMinistries' className="item">242 MINISTRIES</AnchorLink>
-                    <AnchorLink  offset='80' href="#Footer" className="item">CONTACT</AnchorLink>
+                    <AnchorLink offset='100' href='#CustomerSlider' className="item">HOME</AnchorLink>
+                    <AnchorLink offset='80' href='#AboutUs' className="item">ABOUT US</AnchorLink>
+                    <AnchorLink offset='80' href='#Equipping' className="item">EQUIPPING</AnchorLink>
+                    <AnchorLink offset='80' href='#Evangelism' className="item">EVENT SPEAKING</AnchorLink>
+                    <AnchorLink offset='80' href='#TwoFourTwoMinistries' className="item">242 MINISTRIES</AnchorLink>
+                    <AnchorLink offset='80' href="#Footer" className="item">CONTACT</AnchorLink>
                 </div>
             </div>
         </nav>
